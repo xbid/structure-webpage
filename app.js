1658aa461df40653a9df31d717f58682bd391770
xbid <abdzubaid@gmail.com>
2021-01-27 15:26:31 +0200

    updating the file of java script

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// var userInput = prompt('Whats your name?');
-// console.log(userInput);
 
-// alert('Hello ' + userInput);
+var userInput = prompt('Whats your name?');
+console.log(userInput);
+
+alert('Hello ' + userInput);
+
 
 // console.log(123 + 345);
 
@@ -63,28 +65,30 @@
 //     alert('We  are here in the else')
 // }
 
-// var user = prompt('what is your age?');
-// // people less than 18 to be alerted with hello teenger
+//var user = prompt('what is your age?');
+// people less than 18 to be alerted with hello teenager
 // if(user<18){
 //     alert('Hello Teenger');
-// }else{
-//     alert('Hello Adults');
-//     // as genral case 
-// };
+//}else if(user>18){
+//     alert('Hello Adult');
+//  };
+//     // as general case 
+
 // people greater than 18 to be alerted with hello adults 
 
 // if we have more than one condition
 // 24
+
 var hourNow = prompt('what is the time');
 if(hourNow > 18){
-    document.write('<h3>'+'Good Evening' + '</h3>');
+    document.write('<h2>'+'Good Evening' + '</h2>');
 }else if(hourNow > 12){
-    document.write('Good afternoon');
+    document.write('<h2>'+'Good afternoon'+'</h2>');
 }else if (hourNow >= 0 ){
-    document.write('Good morning');
+    document.write('<h2>'+'Good morning'+'</h2>');
 }else{
-    alert('Welcome!');
+    alert('<h2>'+'Welcome!'+'</h2>');
 }
 
-// another way to show an output
+//another way to show an output
 // document.write("Welcome People");
