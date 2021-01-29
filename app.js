1f6ed66118a85857fcdd250e2115bf52676c51ef
xbid <abdzubaid@gmail.com>
2021-01-29 03:30:08 +0200

    updating the code

--- app.js
+++ app.js
@@ -1,109 +1,36 @@
 
-//var userInput = prompt('What do you do for living');
-//    console.log(userInput);
-// alert('Ok you are a ' + userInput + ' Thats Amazing');
+var userInput = prompt('What do you do for living');
+   console.log(userInput);
+alert('Ok you are a ' + userInput + ' Thats Amazing');
 
-//var marks = prompt('What Mark did you get in your tasks');
-//if(marks >= 100 ){
-//    document.write('<h2>'+' Thats Great I hope to get that mark too in the end'+'</h2>');
-//}else if(marks < 100){
-//    document.write('<h2>'+'Dont quit just yet Study hard keep searching and you will get more than that '+'</h2>');
-//}else if (mark = 0 ){
-//    document.write('<h2>'+'what is this are you sleeping during the class get up i will help you'+'</h2>');
-//}else{
- //   alert('<h2>' + 'Welcome! you can get what ever you want if you just focus' + ' </h2>');
-//};
+var marks = prompt('What Mark did you get in your tasks');
+if(marks >= 100 ){
+   document.write('<h2>'+' Thats Great I hope to get that mark too in the end'+'</h2>');
+}else if(marks < 100){
+   document.write('<h2>'+'Dont quit just yet Study hard keep searching and you will get more than that '+'</h2>');
+}else if (mark = 0 ){
+   document.write('<h2>'+'what is this are you sleeping during the class get up i will help you'+'</h2>');
+}else{
+   alert('<h2>' + 'Welcome! you can get what ever you want if you just focus' + ' </h2>');
+};
 
-//var input = document.getElementById('color');
-//var button = document.querySelector('button');
-//button.addEventListener('click', function( event ) {
-//   event.preventDefault();    
-//   var color = input.value;
-//   document.body.style.background = color;
-//});
+var input = document.getElementById('color');
+var button = document.querySelector('button');
+button.addEventListener('click', function( event ) {
+  event.preventDefault();    
+  var color = input.value;
+  document.body.style.background = color;
+});
 
-//var color = confirm('Do you prefer to change page color ?');
+var color = confirm('Do you prefer to change page color ?');
 
-//if(color == true){
-    //var switchColor = prompt('Type in any color you want the page to turn on!','the color');
-  //  document.body.style.backgroundColor = switchColor;
-//}else{
-  // alert('Why You didnt change the color anyway its up to you, Come on in you are welcome');
-//};
+if(color == true){
+    var switchColor = prompt('Type in any color you want the page to turn on!','the color');
+   document.body.style.backgroundColor = switchColor;
+}else{
+  alert('Why You didnt change the color anyway its up to you, Come on in you are welcome');
+};
 
 // for(var i=0 ; i<10 ; i++){
 //    console.log(i);
-// }
-
-// var userName = prompt("What is your age?");
-
-// if (userName <= 17) {
-//   alert("Welcome " + userName);
-// } else {
-//   alert("Welcome user");
-}
-// document.write('<h1>'+ 'Welcome '+userName+'</h1>'+'<br>');
-// function myFunction() {
-//   var s = confirm("Confirm it");
-//   if (s == true) {
-//     document.write(
-//       '<h4 style="color:blue;">' +
-//         "You Confirm the request " +
-//         userName +
-//         " !!" +
-//         "</h4>" +
-//         "<br>" +
-//         '<a href="index.html">' +
-//         "Back" +
-//         "</a>"
-//     );
-//   } else {
-//     document.write(
-//       '<h4 style="color:red;">' +
-//         "You Decline the request " +
-//         userName +
-//         " !!" +
-//         "</h4>" +
-//         "<br>" +
-//         '<a href="index.html">' +
-//         "Back" +
-//         "</a>"
-//     );
-//   }
-// }
-
-// function Age() {
-//   var age = prompt("Your Age");
-// if (isNaN(age) || age <= 0 || age > 100){
-//     alert('please enter a valid number')
-//     Age()
-// }else{
-//     if (age < 18) {
-//       alert("Hello Teenger");
-//     } else {
-//       alert("Hello Adults");
-//     }
-// }
-// }
-// function changeColor(){
-//     var articleColor=prompt('in Which Color you want to see the Article');
-//     document.getElementById("article").style.color = articleColor;
-
-// }
-
-
-// function CatCount(){
-//   var count =prompt('How Many Cat Do You have');
-//   if(isNaN(count)){
-//      CatCount()
-//   } else {
-//     var i=1;
-//     while(count!=0){
-//       document.write('<h1>'+i+'</h1>');
-//       i++;
-//       document.write('<img style="width:250px;height:250px" src="https://mohdazzam.github.io/Lab-03b/img/cat.jpg" />'+'<br>');
-//       count--;
-      
-//     }
-//   }
 // }
--- app2.js
+++ app2.js
@@ -6,38 +6,37 @@ while(!(i=="Mansaf" || i=="Makmoora" || i=="Burger")){
 i=prompt("Enter Your type")
 }
 var c= prompt("How often do you Eat " + i)
-
 var image = " ";
 
 
-//for(var j = 0; j < x; j++){
+for(var j = 0; j < x; j++){
     
-  //  if(i == "quran"  ){
-    //    image = image + '<img src"">'
-      //  console.log(image)
-    //}else if(i =="books"){
-      //  image = image + '<img src""> '
-        //console.log(image)
-    //}else if(i == "magazines"){
+   if(i == "Mansaf"  ){
+       image = image + '<img src"">'
+       console.log(image)
+    }else if(i =="Makmoora"){
+       image = image + '<img src""> '
+        console.log(image)
+    }else if(i == "Burger"){
 
-      //  image = image + '<img src""> '
-        //console.log(image)
-    //}
+       image = image + '<img src""> '
+        console.log(image)
+    }
     
     
-//     for(var j = 0; j < x; j++){
-//        if(i =='quran'){
-//             image = image + '<img src="s.jfif">'
-//             console.log(image)
+    for(var j = 0; j < x; j++){
+       if(i =='Mansaf'){
+            image = image + '<img src="https://www.cheftariq.com/wp-content/uploads/2020/04/mansaf-4-1-500x500.jpg">'
+            console.log(image)
             
-//         }else if(i =="books"){
-//             image = image +'<img src="a.jfif">'
+        }else if(i =="Makmoora"){
+            image = image +'<img src="https://theculturetrip.com/wp-content/uploads/2017/03/b49d1b867772f191a0dbcda19596982aef8428cd.jpg">'
       
-//         }else if(i =="magazines"){
-//             image = image +'<img src="b.jpg">' 
-//     }
+        }else if(i =="buger"){
+            image = image +'<img src="https://media-cdn.tripadvisor.com/media/photo-s/17/ba/a6/31/burger.jpg">' 
+    }
     
     
-//   }
+  }
 
-// document.write(image);
+document.write(image);
